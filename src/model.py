e2b9cbe6a77af7f202bf4d524acebfdfddb2c59e
Benjamin Bay <bay1@ip-172-31-25-194.ec2.internal>
2019-03-11 06:00:10 +0000

    Added function for hdr deconv layers.

--- src/model.py
+++ src/model.py
@@ -46,7 +46,7 @@ def train_model(dataset):
     X, Y = dataset
     model.summary()
     model.fit(X, Y, epochs=args.num_epochs, batch_size=args.batch_size,
-        verbose=2, validation_split=0.2, shuffle=True)
+              verbose=2, validation_split=0.2, shuffle=True)
 
 
 def custom_loss(yTrue, yPred):
@@ -54,7 +54,7 @@ def custom_loss(yTrue, yPred):
 
 
 def psnr(yTrue, yPred):
-    return 20 * K.log( 1 / K.mean(K.square(yTrue - yPred)) ) / 2.3
+    return 20 * K.log(1 / K.mean(K.square(yTrue - yPred))) / 2.3
 #    print(mse)
 #    if mse == 0:
 #        return 100
@@ -63,19 +63,27 @@ def psnr(yTrue, yPred):
 
 
 def ldr_encoder():
-    vgg16_input = VGG16(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+    vgg16_input = VGG16(
+        include_top=False,
+        weights='imagenet',
+        input_shape=(224, 224, 3))
     for layer in vgg16_input.layers[:17]:
         layer.trainable = False
     result = vgg16_input.layers[-2].output
     return result, vgg16_input
 
 
+def decoder_layer(nn_in, n_filters, filter_size, stride, pad="same", act="linear"):
+    nn = Conv2DTranspose(n_filters, filter_size, strides=stride, padding=pad, activation=act)(nn_in)
+    return nn
+
+
 def hdr_decoder(latent_rep):
-    network = Conv2DTranspose(256, (3, 3), strides=(2, 2), padding='same')(latent_rep)
-    network = Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same')(network)
-    network = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same')(network)
-    network = Conv2DTranspose(32, (3, 3), strides=(2, 2), padding='same')(network)
-    network = Conv2DTranspose(3, (1, 1), activation='sigmoid', padding='same')(network)
+    network = decoder_layer(latent_rep, 256, 3, 2)
+    network = decoder_layer(network, 128, 3, 2)
+    network = decoder_layer(network, 64, 3, 2)
+    network = decoder_layer(network, 32, 3, 2)
+    network = decoder_layer(network, 3, 1, 1, act='sigmoid')
     return network
 
 
@@ -86,9 +94,9 @@ def assemble_model():
     # Decoder
     x = hdr_decoder(latent_rep)
 
-    model = Model(inputs=vgg16_input.input, outputs=x)    
+    model = Model(inputs=vgg16_input.input, outputs=x)
     model.compile(optimizer='adam', loss=custom_loss,
-        metrics=[psnr])
+                  metrics=[psnr])
 
     return model
 
@@ -96,11 +104,11 @@ def assemble_model():
 def load_datasets():
     x_fd = os.path.join(args.input_dir, 'x')
     y_fd = os.path.join(args.input_dir, 'y')
-    
+
     x = []
     y = []
     for i, imgx_fn in enumerate(os.listdir(x_fd)):
-        if (args.max_samples is not None) and (i+1 > args.max_samples):
+        if (args.max_samples is not None) and (i + 1 > args.max_samples):
             break
         imgx_fp = os.path.join(x_fd, imgx_fn)
         imgy_fn = 'y' + imgx_fn[1:]
@@ -109,14 +117,14 @@ def load_datasets():
             assert(os.path.isfile(imgy_fp))
             x.append(image.img_to_array(image.load_img(imgx_fp)))
             y.append(image.img_to_array(image.load_img(imgy_fp)))
-    
+
     x = np.asarray(x) / 255
     y = np.asarray(y) / 255
 
     # Normalize input images
     #x = preprocess_input(x)
     #y = preprocess_input(y)
-    
+
     # Shuffle training pairs
     idx = list(range(x.shape[0]))
     np.random.shuffle(idx)
@@ -125,6 +133,6 @@ def load_datasets():
 
     return x, y
 
+
 if __name__ == '__main__':
     main()
-
